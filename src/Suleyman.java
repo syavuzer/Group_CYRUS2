23d123927f714e89a90ce6d66df41578ff44adba
syavuzer <yavuzercolorado@hotmail.com>
2023-01-09 16:37:34 -0700

    added new message

--- src/Suleyman.java
+++ src/Suleyman.java
@@ -1,6 +1,7 @@
 public class Suleyman {
     public static void main(String[] args) {
         System.out.printf("Our second group practice");
+        System.out.println("We will be better then yesterday");
 
 
     }
