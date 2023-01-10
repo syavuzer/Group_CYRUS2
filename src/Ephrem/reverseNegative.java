24b476b5023f729eb233b7be13d2608dff89d00c
emphi73 <emesfin73@yahoo.com>
2023-01-10 01:07:24 -0600

    new branch Ephrem and new classes primeNumbers and reverseNegative created

---
+++ src/Ephrem/reverseNegative.java
@@ -0,0 +1,23 @@
+package Ephrem;
+import java.util.Scanner;
+
+public class reverseNegative {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter a negative number");
+        int negative = input.nextInt();
+        if (negative>=0){
+            System.err.println("only negative number accepted");
+            System.out.println("Enter a negative number");
+            negative = input.nextInt();
+        }
+
+        System.out.println(negativeNum(negative));
+    }
+    public static String negativeNum(int negative){
+        int absNegative = Math.abs(negative);
+        //int absNegative = -1*negative; we can also use this
+
+        return "The absolute value of "+negative+" is " + absNegative;
+    }
+}
