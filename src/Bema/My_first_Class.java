ca84ebe4627374e6146b98385f4f098b0f7865cd
akbermetmuratova <Bemaibragimovna@gmail.com>
2023-01-10 20:20:28 -0600

    created firts class

---
+++ src/Bema/My_first_Class.java
@@ -0,0 +1,10 @@
+package Bema;
+
+public class My_first_Class {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("SDLC ==> Software Development Life Cycle");
+    }
+}
