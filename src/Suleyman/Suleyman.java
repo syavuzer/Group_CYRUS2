f78f316e50c3916d5a43b9255bca9eabdaa13362
syavuzer <yavuzercolorado@hotmail.com>
2023-01-10 19:19:31 -0700

    fixed coding problem

--- src/Suleyman/Suleyman.java
+++ src/Suleyman/Suleyman.java
@@ -1,12 +1,10 @@
 package Suleyman;
 
 public class Suleyman {
+
     public static void main(String[] args) {
-        System.out.println("Our second group practice");
+        System.out.println("We are the best team");
 
         System.out.println("We will be better then yesterday");
-
-
     }
-
 }
