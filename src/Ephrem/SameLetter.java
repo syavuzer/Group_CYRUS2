dc7253754af865fdd245de4016857135fdc6a3a3
emphi73 <emesfin73@yahoo.com>
2023-01-14 15:58:44 -0600

    removing duplicate class

---
+++ src/Ephrem/SameLetter.java
@@ -0,0 +1,38 @@
+package Ephrem;
+import java.util.Scanner;
+
+public class SameLetter {
+    public static boolean sameLetter(String firstWord, String secondLetter){
+        boolean same = true;
+        for(int i=0; i<firstWord.length(); i++){
+            int count = 0;
+            for(int j=0; j<firstWord.length(); j++) {
+                if (firstWord.charAt(i) == secondLetter.charAt(j)) {
+                    count++;
+                    break;
+                }
+            }
+            if(count == firstWord.length()-1){
+                same = true;
+            }
+        }
+       return same;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your first word");
+        String firstWord = input.next();
+        System.out.println("Enter your second word");
+        String secondWord = input.next();
+
+        System.out.println(sameLetter(firstWord, secondWord));
+    }
+}
+/*
+String -- Same letters
+Write a return method that check if a string is build out of the
+same letters as another string.
+Ex: same("abc", "cab"); -> true
+same("abc", "abb"); -> false:
+ */
