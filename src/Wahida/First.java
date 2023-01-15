336a4d7b568ac1f26011ab06171b3fae3df87334
syavuzer <yavuzercolorado@hotmail.com>
2023-01-15 13:03:26 -0700

    fixed coding problem

--- src/Wahida/First.java
+++ src/Wahida/First.java
@@ -1,8 +1,10 @@
+package Wahida;
+
 public class First {
 
     public static void main(String[] args) {
 
-        System.out.println("First Class");
+        System.out.println("Wahida.First Class");
     }
 
 }
