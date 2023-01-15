e488f85e842f652156ee199d7ecb69852e955aa5
emphi73 <emesfin73@yahoo.com>
2023-01-15 10:48:08 -0600

    new classes

---
+++ src/Ephi/RemoveDuplicate.java
@@ -0,0 +1,31 @@
+package Ephi;
+
+import java.util.Scanner;
+
+public class RemoveDuplicate {
+    public static String duplicate(String word){
+        String unique = "";
+        for(int i=0; i<word.length(); i++){
+            if(i == word.length()-1 && !unique.contains(String.valueOf(word.charAt(i)))){
+                unique+=word.charAt(i);
+            }else if(Character.toLowerCase(word.charAt(i))!= word.charAt(i+1)){
+                unique+=word.charAt(i);
+            }
+        }
+        return unique;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = input.next();
+
+        System.out.println(duplicate(word));
+    }
+}
+/*
+String -- Remove Duplicates
+Write a return method that can remove the duplicated values from
+String
+Ex: removeDup("AAABBBCCC") ==> ABC
+ */
