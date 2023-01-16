f76d250311dcb59037369bea170ca6fa00833272
emphi73 <emesfin73@yahoo.com>
2023-01-16 14:40:00 -0600

    update same letters class

--- src/Ephrem/SameLetter.java
+++ src/Ephrem/SameLetter.java
@@ -32,7 +32,7 @@ public class SameLetter {
     }
 }
 /*
-String -- Same letters
+String --- Same letters
 Write a return method that check if a string is build out of the
 same letters as another string.
 Ex: same("abc", "cab"); -> true
