bedd8b3edae7748d1db9ae27696d2c9c70784d50
emphi73 <emesfin73@yahoo.com>
2023-01-18 18:51:00 -0600

    update same letter class

--- src/Ephrem/SameLetter.java
+++ src/Ephrem/SameLetter.java
@@ -17,9 +17,9 @@ public class SameLetter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your first word");
-        String firstWord = input.next();
+        String firstWord = input.nextLine();
         System.out.println("Enter your second word");
-        String secondWord = input.next();
+        String secondWord = input.nextLine();
 
         System.out.println(sameLetter(firstWord, secondWord));
     }
