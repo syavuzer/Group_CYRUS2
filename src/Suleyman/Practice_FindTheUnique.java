a6d0bf521b1548688b94aeeb9ddfd7590f4b01a7
syavuzer <yavuzercolorado@hotmail.com>
2023-01-24 08:45:10 -0700

    added new class

--- src/Suleyman/Practice_FindTheUnique.java
+++ src/Suleyman/Practice_FindTheUnique.java
@@ -3,6 +3,22 @@ package Suleyman;
 public class Practice_FindTheUnique {
     public static void main(String[] args) {
 
+        String str = "AAABBBCCCDEF";
+
+        String unique = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char each = str.charAt(i);
+
+            if(str.indexOf(each) == str.lastIndexOf(each)  ){
+                unique += each;
+            }
+
+        }
+
+
+        System.out.println(unique);
+
     }
 
 
