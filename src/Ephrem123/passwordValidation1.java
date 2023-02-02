4d6b2e242fffe71f2d9c156735ed25bc75274c79
emphi73 <emesfin73@yahoo.com>
2023-02-02 10:44:02 -0600

    new classes

---
+++ src/Ephrem123/passwordValidation1.java
@@ -0,0 +1,39 @@
+package Ephrem123;
+
+import java.util.Scanner;
+
+public class passwordValidation1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your password (must be at least six character, must contain one capital letter, "
+                +"one lower letter, one digit and one special character)");
+        String password = input.next();
+
+        System.out.println(validatePassword(password));
+    }
+    public static String validatePassword(String password){
+        String passCreate = "";boolean isCapital = false , isLower = false , isSpecial = false, isDigit = false;
+        if (password.length()>=6){
+            for(int i=0; i<password.length(); i++){
+                if(Character.isUpperCase(password.charAt(i))){
+                    isCapital = true;
+                }else if(Character.isLowerCase(password.charAt(i))){
+                    isLower = true;
+                }else if(Character.isDigit(password.charAt(i))){
+                    isDigit = true;
+                }else if(!Character.isLetter(password.charAt(i)) && !Character.isDigit(password.charAt(i))){
+                    isSpecial = true;
+                }
+            }
+            passCreate = (isCapital && isLower && isDigit && isSpecial )?"Password created successfully":
+                    (!isCapital)?"password should contain at least one UpperCase":(!isLower)?"password should contain "+
+                            "at least one lowerCase":(!isDigit)?"password should contain at least one Digit":
+                            "password should contain at least one Special character";
+
+        }else {
+            passCreate = "password should have six characters.";
+        }
+
+        return passCreate;
+    }
+}
---
+++ src/Ephrem123/sumOfDigits1.java
@@ -0,0 +1,23 @@
+package Ephrem123;
+
+import java.util.Scanner;
+
+public class sumOfDigits1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word that contains digits.");
+        String word = input.next();
+
+        System.out.println(sumDigits(word));
+    }
+    public static int sumDigits(String word){
+        int num = 0;
+        for (int i=0; i<word.length(); i++) {
+            if(Character.isDigit(word.charAt(i))){
+                //num+=Integer.parseInt(String.valueOf(word.charAt(i)));
+                num+= Character.getNumericValue(word.charAt(i));
+            }
+        }
+        return num;
+    }
+}
